996eac1f11460990bb3c602f8a4be07d2c91ebd7
App Academy Student <student@appacademy.io>
2015-04-29 10:25:44 -0700

    Redoing DOMNode collection and selectors

--- jquery_lite.js
+++ jquery_lite.js
@@ -1,15 +1,26 @@
 (function(){
 
-  if (typeof jqueryLite === "undefined") {
-    window.jqueryLite = {};
+  var $l = window.$l =  function(selector){
+      var nodes =  Array.prototype.slice.call(document.querySelectorAll(selector));
+      return new DOMNodeCollection(nodes);
+  };
+
+  var DOMNodeCollection = function(elementsArray){
+      this.HTMLElements = elementsArray;
+  };
+
+  DOMNodeCollection.prototype.html = function(string) {
+    var innerHTML;
+
+    if (!string) {
+      return this.HTMLElements[0].innerHTML;
+    } else {
+      this.HTMLElements.forEach(function (node) {
+        node.innerHTML = string;
+      });
+    }
   }
 
-  var $l = jqueryLite.$l =  function(selector){
-      return Array.prototype.slice.call(document.querySelectorAll(selector));
-  }
 
-  var DOMNodeCollection = jqueryLite.DOMNodeCollection = function(elementsArray){
-      this.HTMLElements = elementsArray; 
-  }
 
 })();
