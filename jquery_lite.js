e8043f64f6eb0b97e18f5bf7a7172e1343a5448e
App Academy Student <student@appacademy.io>
2015-04-29 10:59:20 -0700

    Add #attr

--- jquery_lite.js
+++ jquery_lite.js
@@ -1,8 +1,13 @@
 (function(){
 
   var $l = window.$l =  function(selector){
-      var nodes =  Array.prototype.slice.call(document.querySelectorAll(selector));
-      return new DOMNodeCollection(nodes);
+      if (selector instanceof HTMLElement){
+        var node = [selector]
+        return new DOMNodeCollection(node);
+      }else{
+        var nodes =  Array.prototype.slice.call(document.querySelectorAll(selector));
+        return new DOMNodeCollection(nodes);
+      }
   };
 
   var DOMNodeCollection = function(elementsArray){
@@ -19,13 +24,26 @@
         node.innerHTML = string;
       });
     }
-  }
+  };
 
   DOMNodeCollection.prototype.empty = function() {
     this.HTMLElements.forEach(function(node) {
       node.innerHTML = "";
     });
-  }
+  };
+
+  DOMNodeCollection.prototype.attr = function(attributeName, value) {
+    if (!value){
+      return this.HTMLElements[0].getAttribute(attributeName)
+    } else {
+      this.HTMLElements.forEach(function(node){
+        node.setAttribute(attributeName, value);
+      });
+      return "attributes changed...";
+    };
+
+    return "something wrong happened";
+  };
 
 
 
